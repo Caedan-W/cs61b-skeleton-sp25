4ad8e72c2dc384540e74d6e6f0950ee2a68878a7
caedan <421155149@qq.com>
2025-07-14 15:51:14 +0800

    complete Maximizer

--- proj1b/src/deque/Maximizer61B.java
+++ proj1b/src/deque/Maximizer61B.java
@@ -1,5 +1,6 @@
 package deque;
 import java.util.Comparator;
+import java.util.Iterator;
 
 public class Maximizer61B {
     /**
@@ -10,7 +11,18 @@ public class Maximizer61B {
      * @return          the maximum element
      */
     public static <T extends Comparable<T>> T max(Iterable<T> iterable) {
-        return null;
+        Iterator<T> it = iterable.iterator();
+        if (!it.hasNext()) {
+            return null;
+        }
+        T maxItem = it.next();
+        while (it.hasNext()) {
+            T x = it.next();
+            if (x.compareTo(maxItem) > 0) {
+                maxItem = x;
+            }
+        }
+        return maxItem;
     }
 
     /**
@@ -22,17 +34,28 @@ public class Maximizer61B {
      * @return          the maximum element according to the comparator
      */
     public static <T> T max(Iterable<T> iterable, Comparator<T> comp) {
-        return null;
+        Iterator<T> it = iterable.iterator();
+        if (!it.hasNext()) {
+            return null;
+        }
+        T maxItem = it.next();
+        while (it.hasNext()) {
+            T x = it.next();
+            if (x != null && comp.compare(x, maxItem) > 0){
+                maxItem = x;
+            }
+        }
+        return maxItem;
     }
 
     public static void main(String[] args) {
         // The style checker will complain about this main method, feel free to delete.
 
-        // ArrayDeque61B<Integer> ad = new ArrayDeque61B<>();
-        // ad.addLast(5);
-        // ad.addLast(12);
-        // ad.addLast(17);
-        // ad.addLast(23);
-        // System.out.println(max(ad));
+         ArrayDeque61B<Integer> ad = new ArrayDeque61B<>();
+         ad.addLast(5);
+         ad.addLast(12);
+         ad.addLast(17);
+         ad.addLast(23);
+         System.out.println(max(ad));
     }
 }
